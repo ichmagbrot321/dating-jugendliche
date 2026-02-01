8076917744c2209c2086081e8874b0dbb8887a69
ichmagbrot321 <pajaziti.leon97080@gmail.com>
2026-02-01 20:59:58 +0100

    Enhance service worker with skipWaiting and claim

--- dating-pwa/public/service-worker.js
+++ dating-pwa/public/service-worker.js
@@ -11,6 +11,7 @@ const ASSETS = [
 
 // Install
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
@@ -21,15 +22,20 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
       )
     )
   );
+  self.clients.claim();
 });
 
 // Fetch
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    caches.match(event.request).then(
+      cached => cached || fetch(event.request)
+    )
   );
 });
